2cc9820bec3c37f93e1613c7223a7d9031a4c548
dinathebanana <dqphung101@student.hbuhsd.edu>
2025-10-21 21:46:22 +0000

    w

--- Main.java
+++ Main.java
@@ -4,6 +4,15 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // Problem 1
+    for (int = 1; i <= 25; i += 2)
+    {
+      System.out.print(i + " ");
+    }
+    // Problem 2
+    for (i = 17; i <= 73; i+=1 )
+    {
+      System.out.print()
+    }
   }
 }
